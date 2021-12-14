81eaebcf151d870336162525bd15cdaf25b543e7
Marcus Bakke <marcusabakke@gmail.com>
2021-12-13 21:29:35 -0800

    Add lightning effect to title screen

--- scenes/title_scene.lua
+++ scenes/title_scene.lua
@@ -10,110 +10,85 @@ function titleScene:load()
     self.title = {}
     self.title.image = peachy.new(self.BG_SpriteSheetMeta, self.BG_SpriteSheet, "Idle")
     self.title.x = -WindowWidth/GlobalScale
+    self.lightning = {}
+    self.lightning.image = peachy.new(self.BG_SpriteSheetMeta, self.BG_SpriteSheet, "lightning")
+    self.lightning.image:pause()
+    self.lightning.timer = 0.0
+    self.lightning.duration = 1.0
+    self.lightning.alpha = 1.0
+    self.lightning.delay = 4.35
+    self.lightning.trigger = false
+    self.flash = false
     self.phrase = {}
     self.phrase[1] = peachy.new(self.BG_SpriteSheetMeta, self.BG_SpriteSheet, "draw")
     self.phrase[2] = peachy.new(self.BG_SpriteSheetMeta, self.BG_SpriteSheet, "your")
     self.phrase[3] = peachy.new(self.BG_SpriteSheetMeta, self.BG_SpriteSheet, "weapon")
-    self.phrase.delay = {1.1, 2.1, 3.1}
+    self.phrase.delay = {1.1, 2.1, 3.1, 4.3}
     self.start_timer = false
     self.timer = 0.0
     self.interact = false
     self.PB_SpriteSheet = love.graphics.newImage("assets/ui/press_button.png")
     self.PB_SpriteSheetMeta = "assets/ui/press_button.json"
     self.press_button = peachy.new(self.PB_SpriteSheetMeta, self.PB_SpriteSheet, "Idle")
-    --self:loadChars("drew", "lilah")
+    local chars = {"drew", "lilah", "sam", "miller", "lilah", "drew"}
+    local x = {WindowWidth/GlobalScale*0.1, WindowWidth/GlobalScale*0.25, WindowWidth/GlobalScale*0.4, WindowWidth/GlobalScale*0.55, WindowWidth/GlobalScale*0.75, WindowWidth/GlobalScale*0.9}
+    self:loadChars(chars, x)
     self.music = love.audio.newSource("assets/audio/music/title.ogg", "static")
     self.sfx_start = love.audio.newSource("assets/audio/sfx/ui/accept_all.ogg", "static")
 end
 
-function titleScene:loadChars(p1, p2)
-    -- Load Character 1
+function titleScene:loadChars(chars, x)
+
+    -- Load Characters
     self.chars = {}
-    self.chars[1] = {}
-    self.chars[1].name = p1
-    self.chars[1].spritesheet = love.graphics.newImage("assets/characters/"..self.chars[1].name..".png")
-    self.chars[1].asepriteMeta = "assets/characters/"..self.chars[1].name..".json"
-    self.chars[1].animation = {
-        sheathed = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "sheathed"),
-        idle = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "idle"),
-        walk = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "walk forward"),
-        jump = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "jump"),
-        airborne = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "airborne"),
-        land = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "land"),
-        hit = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "hit"),
-        block = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "block"),
-        block_start = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "block start"),
-        block_end = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "block end"),
-        a1 = peachy.new(self.chars[1].asepriteMeta, self.chars[1].spritesheet, "attack 1"),
-    }
-    self.chars[1].x = WindowWidth/GlobalScale*0.1
-    self.chars[1].y = WindowHeight/GlobalScale*0.8
-    self.chars[1].xShift = 0
-    self.chars[1].xDir = 1
-    self.chars[1].animationName = "sheathed"
-    require("characters/"..self.chars[1].name)
-    self.chars[1].xorigin = xorigin
-    self.chars[1].block_start_dur = block_start_dur
-    self.chars[1].block_end_dur = block_end_dur
-    self.chars[1].body_width_pad = body_width_pad
-    self.chars[1].body_height_pad = body_height_pad
-    self.chars[1].x_shift_pad = x_shift_pad
-    self.chars[1].idle_duration = idle_duration
-    self.chars[1].attack_1_duration = attack_1_duration
-    self.chars[1].jump_duration = jump_duration
-    self.chars[1].airborne_duration = airborne_duration
-    self.chars[1].land_duration = land_duration
-    self.chars[1].damage_duration = damage_duration
-    self.chars[1].idle = idle
-    self.chars[1].a1 = a1
-    self.chars[1].walk = walk
-    self.chars[1].block_start = block_start
-    self.chars[1].block = block
-    self.chars[1].block_end = block_end
-    -- Load Character 2
-    self.chars[2] = {}
-    self.chars[2].name = p2
-    self.chars[2].spritesheet = love.graphics.newImage("assets/characters/"..self.chars[2].name..".png")
-    self.chars[2].asepriteMeta = "assets/characters/"..self.chars[2].name..".json"
-    self.chars[2].animation = {
-        idle = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "idle"),
-        walk = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "walk forward"),
-        jump = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "jump"),
-        airborne = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "airborne"),
-        land = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "land"),
-        hit = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "hit"),
-        block = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "block"),
-        block_start = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "block start"),
-        block_end = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "block end"),
-        a1 = peachy.new(self.chars[2].asepriteMeta, self.chars[2].spritesheet, "attack 1"),
-    }
-    self.chars[2].x = WindowWidth/GlobalScale*0.2
-    self.chars[2].y = WindowHeight/GlobalScale*0.8
-    self.chars[2].xShift = 0
-    self.chars[2].xDir = -1
-    self.chars[2].animationName = "idle"
-    require("characters/"..self.chars[2].name)
-    self.chars[2].xorigin = xorigin
-    self.chars[2].block_start_dur = block_start_dur
-    self.chars[2].block_end_dur = block_end_dur
-    self.chars[2].body_width_pad = body_width_pad
-    self.chars[2].body_height_pad = body_height_pad
-    self.chars[2].x_shift_pad = x_shift_pad
-    self.chars[2].idle_duration = idle_duration
-    self.chars[2].attack_1_duration = attack_1_duration
-    self.chars[2].jump_duration = jump_duration
-    self.chars[2].airborne_duration = airborne_duration
-    self.chars[2].land_duration = land_duration
-    self.chars[2].damage_duration = damage_duration
-    self.chars[2].idle = idle
-    self.chars[2].a1 = a1
-    self.chars[2].walk = walk
-    self.chars[2].block_start = block_start
-    self.chars[2].block = block
-    self.chars[2].block_end = block_end
+    for i, char in ipairs(chars) do
+        self.chars[i] = {}
+        self.chars[i].name = char
+        self.chars[i].spritesheet = love.graphics.newImage("assets/characters/"..self.chars[i].name..".png")
+        self.chars[i].asepriteMeta = "assets/characters/"..self.chars[i].name..".json"
+        -- sheathed = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "sheathed"),
+        self.chars[i].animation = {
+            idle = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "idle"),
+            walk = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "walk_forward"),
+            jump = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "jump"),
+            airborne = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "airborne"),
+            land = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "land"),
+            hit = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "hit"),
+            block = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "block"),
+            block_start = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "block start"),
+            block_end = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "block end"),
+            a1 = peachy.new(self.chars[i].asepriteMeta, self.chars[i].spritesheet, "attack 1"),
+        }
+        self.chars[i].x = x[i]
+        self.chars[i].y = WindowHeight/GlobalScale*0.8
+        self.chars[i].xShift = 0
+        self.chars[i].xDir = 1
+        self.chars[i].animationName = "idle"
+        self.chars[i].charsheet = require("characters/"..self.chars[i].name)
+        self.chars[i].xorigin = self.chars[i].charsheet.xorigin
+        self.chars[i].yorigin = self.chars[i].charsheet.yorigin
+        self.chars[i].block_start_dur = self.chars[i].charsheet.block_start_dur
+        self.chars[i].block_end_dur = self.chars[i].charsheet.block_end_dur
+        self.chars[i].body_width_pad = self.chars[i].charsheet.body_width_pad
+        self.chars[i].body_height_pad = self.chars[i].charsheet.body_height_pad
+        self.chars[i].x_shift_pad = self.chars[i].charsheet.x_shift_pad
+        self.chars[i].idle_duration = self.chars[i].charsheet.idle_duration
+        self.chars[i].attack_i_duration = self.chars[i].charsheet.attack_1_duration
+        self.chars[i].jump_duration = self.chars[i].charsheet.jump_duration
+        self.chars[i].airborne_duration = self.chars[i].charsheet.airborne_duration
+        self.chars[i].land_duration = self.chars[i].charsheet.land_duration
+        self.chars[i].damage_duration = self.chars[i].charsheet.damage_duration
+        self.chars[i].idle = self.chars[i].charsheet.idle
+        self.chars[i].a1 = self.chars[i].charsheet.a1
+        self.chars[i].walk = self.chars[i].charsheet.walk
+        self.chars[i].block_start = self.chars[i].charsheet.block_start
+        self.chars[i].block = self.chars[i].charsheet.block
+        self.chars[i].block_end = self.chars[i].charsheet.block_end
+    end
 end
 
 function titleScene:update(dt, gameState)
+    print(self.lightning.timer)
     if self.title.x == 0 and not self.music:isPlaying() then
         self.music:play()
     end
@@ -121,11 +96,12 @@ function titleScene:update(dt, gameState)
         ResetInputs()
     end
     self:updateTitle(dt)
-    --self:updateChars(dt)
+    self:updateLightning(dt)
+    self:updateChars(dt)
     for i = 1, 3 do
         self.phrase[i]:update(dt)
     end
-    if self.timer > self.phrase.delay[3] + 1 then
+    if self.timer > self.phrase.delay[4] then
       self.press_button:update(dt)
     end
 
@@ -147,44 +123,59 @@ function titleScene:update(dt, gameState)
 end
 
 function titleScene:updateChars(dt)
-    self.chars[1].animation[self.chars[1].animationName]:update(dt)
-    self.chars[2].animation[self.chars[2].animationName]:update(dt)
+    for i = 1, #self.chars do
+        self.chars[i].animation[self.chars[i].animationName]:update(dt)    
+    end
 end
 
 function titleScene:draw(sx, sy)
     love.graphics.push()
     love.graphics.scale(sx, sy)
-    love.graphics.setBackgroundColor(0.0, 0.0, 0.0, 1.0)
-    self.title.image:draw(self.title.x, 0)
+    love.graphics.setBackgroundColor(0.05, 0.05, 0.05, 1.0)
+    self:drawLightning()
+    self.title.image:draw(self.title.x, -WindowHeight/GlobalScale*0.1)
+    love.graphics.setColor(0.2, 0.2, 0.2, 1)
+    love.graphics.rectangle("fill", 0, WindowHeight/GlobalScale*0.8, WindowWidth/GlobalScale, WindowHeight/GlobalScale)
     love.graphics.setColor(1, 1, 1, 1)
     for i = 1, 3 do
         if self.timer > self.phrase.delay[i] then
-            self.phrase[i]:draw()
+            self.phrase[i]:draw(0.0, -WindowHeight/GlobalScale*0.1)
         end
     end
-    if self.timer > self.phrase.delay[3] + 1.05 then
-        self.press_button:draw(WindowWidth/GlobalScale/2, WindowHeight/GlobalScale*0.8, 0, 1, 1, self.press_button:getWidth()/2, self.press_button:getHeight()/2)
-        self.interact = true
+    if self.interact then
+        self.press_button:draw(WindowWidth/GlobalScale/2, WindowHeight/GlobalScale*0.6, 0, 1, 1, self.press_button:getWidth()/2, self.press_button:getHeight()/2)
     end
-    --self:drawChars()
+    self:drawChars()
+    if self.flash then
+        self:drawFlash()
+    end
     love.graphics.pop()
 end
 
 function titleScene:drawChars()
-    self.chars[1].animation[self.chars[1].animationName]:draw(self.chars[1].x,
-                                                              self.chars[1].y,
-                                                              0,
-                                                              self.chars[1].xDir,
-                                                              1,
-                                                              self.chars[1].xorigin,
-                                                              self.chars[1].animation[self.chars[1].animationName]:getHeight()/2)
-    self.chars[2].animation[self.chars[2].animationName]:draw(self.chars[2].x,
-                                                              self.chars[2].y,
-                                                              0,
-                                                              self.chars[2].xDir,
-                                                              1,
-                                                              self.chars[2].xorigin,
-                                                              self.chars[2].animation[self.chars[2].animationName]:getHeight()/2)
+    local counter = 1
+    for i = 1, #self.chars do
+        if self.timer > self.phrase.delay[counter] then
+            self.chars[i].animation[self.chars[i].animationName]:draw(self.chars[i].x,
+                                                                    self.chars[i].y,
+                                                                    0,
+                                                                    2*math.fmod(i,2)-1,
+                                                                    1,
+                                                                    self.chars[i].xorigin,
+                                                                    self.chars[i].animation[self.chars[i].animationName]:getHeight()-self.chars[i].yorigin)
+        end
+        if math.fmod(i, 2) ~= 1 then
+            counter = counter + 1
+        end
+    end
+end
+
+function titleScene:drawLightning()
+    if self.lightning.trigger then
+        love.graphics.setColor(1.0, 1.0, 1.0, self.lightning.alpha)
+        self.lightning.image:draw(0, 0)
+        love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
+    end
 end
 
 function titleScene:updateTitle(dt)
@@ -196,9 +187,58 @@ function titleScene:updateTitle(dt)
     end
 end
 
+function titleScene:drawFlash()
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.rectangle("fill", 0, 0, WindowWidth/GlobalScale, WindowHeight/GlobalScale)
+end
+
+function titleScene:updateLightning(dt)
+    if self.lightning.trigger then
+        local old_frame = self.lightning.image:getFrame()
+        self.lightning.image:update(dt)
+        local new_frame = self.lightning.image:getFrame()
+        if new_frame < old_frame then
+            self.lightning.image:setFrame(4)
+        end
+        if self.lightning.timer < 0.2 then
+            self.lightning.alpha = 1.0
+        else
+            self.lightning.alpha = 1.0 - 1.2*(self.lightning.timer / self.lightning.duration)
+        end
+    end
+end
+
 function titleScene:incrementTimers(dt)
+    local previous_trigger = self.lightning.trigger
     if self.start_timer then
         self.timer = self.timer + dt
+    end
+    if self.lightning.trigger or self.lightning.timer > 0.0 then
+        self.lightning.timer = self.lightning.timer + dt
+    end
+    if self.timer > self.phrase.delay[4] then
+        if self.lightning.timer == 0.0 then
+            self.lightning.trigger = true
+            self.lightning.image:setFrame(1)
+            self.lightning.image:play()
+        elseif self.lightning.timer > self.lightning.delay then
+            self.lightning.timer = 0.0
+            self.lightning.trigger = true
+            self.lightning.image:setFrame(1)
+            self.lightning.image:play()
+        elseif self.lightning.timer > self.lightning.duration then
+            self.lightning.trigger = false
+        end
+        self.interact = true
+        for i = 1, #self.chars do
+            self.chars[i].animationName = "a1"
+        end
+    end
+    local new_trigger = self.lightning.trigger
+    if not previous_trigger and new_trigger then
+        self.flash = true
+    else
+        self.flash = false
     end
 end
 
