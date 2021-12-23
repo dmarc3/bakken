8648f00ddbeb38e22b5b300151b690bb453bd46c
Marcus Bakke <marcusabakke@gmail.com>
2021-12-23 10:09:54 -0800

    Add parry noise trigger to Player:damage

--- characters/player.lua
+++ characters/player.lua
@@ -495,6 +495,9 @@ function Player:damage(d)
         self.health = 0
         self.knocked_out = true
     end
+    if self.blocking then
+        self:trigger_sfx("parry")
+    end
     self.hb_anim = true
     self.invuln = true
     self.invuln_timer = 0
